781fed5124727f451adfd9cad17beb96a691512c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-19 00:04:48 +0900

    add: api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Map } from './Map';
 
+const endpoint = process.env.REACT_APP_API_ENDPOINT ? process.env.REACT_APP_API_ENDPOINT : ""
+
+type Pin = {
+  name: string,
+  address: {
+    lat: number,
+    lng: number,
+  }
+}
+
 export const App = () => {
   const [text, setText] = useState<string> ("");
+
+  const [pins, setPins] = useState<Pin[] | null> (null);
+  const getData = async () => {
+    const response: any = await fetch(`${endpoint}/dev/data`)
+    console.log(response.json());
+    setPins([
+      {
+        name: "hoge",
+        address: {
+          lat: 35.69575,
+          lng: 139.77521,
+        },
+      },
+      {
+        name: "fuga",
+        address: {
+          lat: 35.79575,
+          lng: 139.77521,
+        },
+      }
+    ]);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <Box display="flex">
       <Box
@@ -23,7 +60,7 @@ export const App = () => {
           alignItems="center"
           height="80%"
           >
-            <Map setText={setText} />
+            <Map setText={setText} pins={pins} />
           </Box>
       </Box>
       <Box
