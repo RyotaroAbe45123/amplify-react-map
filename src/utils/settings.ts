154efa56be832d2c2365dc1a7708bab2fc40a1dc
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-22 22:29:59 +0900

    add: utils

---
+++ src/utils/settings.ts
@@ -0,0 +1,6 @@
+export const endpoint = process.env.REACT_APP_API_ENDPOINT ? process.env.REACT_APP_API_ENDPOINT : ""
+
+export const initialCenterPostion = {
+    lat: 35.69575,
+    lng: 139.77521,
+}
