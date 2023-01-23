623f9453eec0ad480fc5e33351f331efc19ff823
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-23 22:18:46 +0900

    update: key to env

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,8 @@
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import { Dispatch, SetStateAction } from 'react';
 import { Pin } from '../../types/api';
-import { initialCenterPostion } from '../../utils/settings';
+import { initialCenterPostion, key } from '../../utils/settings';
 
-const key = process.env.REACT_APP_KEY ? process.env.REACT_APP_KEY : "";
 
 const containerStyle = {
     width: "90%",
@@ -28,7 +27,8 @@ export const Map = ({ setId, pins, setShow }: Props) => {
       >
         {
             pins && pins.map((data: any) => (
-                <MarkerF key={data.name} position={data.address} label={data.name} onClick={() => {setId(data.id);setShow(true)}} />
+                <MarkerF
+                  key={data.name} position={data.address} label={data.name} onClick={() => {setId(data.id);setShow(true)}} />
             ))
         }
       </GoogleMap>
